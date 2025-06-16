ed0fc1dcbcdaa4670c92dbc81afbaf14747506a2
euidml <97273970+euidml@users.noreply.github.com>
2025-06-16 12:19:46 -0700

    implemented a fixed navbar #6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import logo from './logo.svg';
-import edward from './images/Edward.png'
-import './App.css';
-
+import edward from "./images/Edward.png";
+import "./App.css";
+import NavBar from "./Navbar/Navbar";
 function App() {
   return (
-    <div className="App">
-      <p className='description'>Hi this is Sabriana</p>
-      <img src={edward}/>
+    <div>
+      <NavBar />
+      <div className="App">
+        <p className="description">Hi this is Sabriana</p>
+        <img src={edward} />
+      </div>
     </div>
   );
 }
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+/* HERE TO IMPORT
+ */
+import './Navbar.css'
+
+function NavBar(){
+    return (
+        <div className="navbar">
+            <a href="#about">About</a>
+            <a href="#experience">Experience</a>
+            <a href="#portfolio">Portfolio</a>
+        </div>
+    );
+}
+
+export default NavBar

2b5e508fcaf826567c64223a389573772fd211ef
euidml <97273970+euidml@users.noreply.github.com>
2025-06-16 10:55:03 -0700

    set up default design: default colour theme, default curve rate in CSS and test it #4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import logo from './logo.svg';
+import circle from './images/circle.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <p className='description'>Hi this is Sabriana</p>
+      <img src={circle}/>
     </div>
   );
 }

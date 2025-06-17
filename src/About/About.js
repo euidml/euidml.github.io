a47d680c28531b5a70b22efe6d1aae4e5807c89e
euidml <97273970+euidml@users.noreply.github.com>
2025-06-17 01:38:12 -0700

    added suitable texts/imgs in About Me #8

--- src/About/About.js
+++ src/About/About.js
@@ -1,10 +1,37 @@
 import { useEffect, useState } from "react";
-import profile from "../images/profile.png";
 import './About.css';
 
+// Import images
+import profile from "../images/profile.png";
+import cv from "../images/cv.svg";
+import facebook from "../images/facebook.svg";
+import instagram from "../images/instagram.svg";
+import linkedin from "../images/linkedin.svg";
+import octicon from "../images/octicon.svg";
 
+
+function ExternalLinks(value){
+
+    return(
+        <div>  
+            <img href={value}>
+            </img>
+        </div>
+    );
+
+}
 
 function About(){
+    const description = ["Hi! Welcome to my personal website. My name is Edward, and I’m a software developer from Vancouver, Canada.",
+                        "I’ve studied Computer Science at Simon Frasers University, and I’m currently seeking my first software developer co-op position. In my free time, I like to travel, watch movies, play instruments.",
+                        "An overview of my past experiences is provided below."]
+    // const imageFiles = [octicon, linkedin, cv, facebook, instagram];
+    const imageFiles = {"octicon": [octicon, "https://github.com/euidml"], 
+                "linkedin": [linkedin, ""],
+                "cv": [cv, ""], 
+                "facebook": [facebook, ""],
+                "instagram": [instagram, ""]
+            };
     return(
         <>
             <div id="about_me">
@@ -13,6 +40,25 @@ function About(){
                 </div>
                 <div id="biography">
                     <h2> About Me 🧑🏻‍💻 </h2>
+                    {
+                        description.map((el) => (
+                            <p key={el}>{el}</p>
+                        ))
+                    }
+                    <div id="tech_stacks">
+                        {/* here is for techstacks */}
+                    </div>
+                    <div id="external_links">
+                        {
+                           Object.keys(imageFiles).map((key) => (
+                                    <a href={imageFiles[key][1]}>
+                                    <img className="external_link" key={key} src={imageFiles[key][0]}/>
+                                    </a>
+                            
+                            ))
+                        }
+                    </div>
+
                 </div>
             </div>
         </>

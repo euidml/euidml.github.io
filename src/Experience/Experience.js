3b7f7c211d338734eb8fc3cfae3b7dd6dc3af048
euidml <97273970+euidml@users.noreply.github.com>
2025-06-17 21:14:49 -0700

    create experiences section and set up it margin/padding #11

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,8 +1,21 @@
-import './Experiece.css';
+import "./Experience.css";
 
 function Experiece(){
     return(
         <>
+            <div id="experience">
+                <div id="work_experience">
+                    <h1>
+                        Work Experiece
+                    </h1>
+                </div>
+                <div id="education">
+                    <h1>
+                        Education
+                    </h1>
+                </div>
+
+            </div>
         </>
     );
 }

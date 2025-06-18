52d0179f856fce4d28b0377cf00dd6cabef9d9a7
euidml <97273970+euidml@users.noreply.github.com>
2025-06-18 00:26:07 -0700

    made innver navbar for category and added overall margins/paddings #14

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,8 +1,35 @@
 import "./Portfolio.css";
 
+function Element({value}){
+    return(
+        <a href={`#${value}`}>
+            {value}
+        </a>
+    );
+}
+
+
+
 function Portfolio(){
+    const categories = ["All Categories", "Web Development", "Game"]
     return(
         <>
+            <div id="portfolio">
+                <h1>
+                    Portfolio💻
+                </h1>
+                <div id="inner_navbar">
+                    {
+                        categories.map((el)=>(
+                            <Element
+                                key={el}
+                                value={el}
+                            />
+                        ))
+                    }
+                </div>
+
+            </div>
         </>
     );
 }
